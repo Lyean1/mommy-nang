6ce30cf6905bcee3f48ce3080428cf5ca24628d9
Lyean1 <lyean1500@gmail.com>
2023-10-06 19:17:34 +0630

    update

--- src/components/Shoppingcart.tsx
+++ src/components/Shoppingcart.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 
 const Shoppingcart: React.FC = () => {
+    const [products, setproducts] = useState<Type>(initialValue);
+
     return (
         <div className="shoppingcart">
             <div>
