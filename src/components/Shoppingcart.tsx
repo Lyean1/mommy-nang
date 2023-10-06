82ddf250a532dbaaf5c34773c9c8fc4a94f2efb3
Lyean1 <lyean1500@gmail.com>
2023-10-06 19:15:07 +0630

    update

--- src/components/Shoppingcart.tsx
+++ src/components/Shoppingcart.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react"
+
 const Shoppingcart: React.FC = () => {
     return (
         <div className="shoppingcart">
             <div>
-                
+
             </div>
         </div>
     )
